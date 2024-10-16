9f79b98745cefadb2d32e8262707853d9d930e3f
María Agustina Romera <maguromera@gmail.com>
2024-10-15 22:24:49 -0300

    creación de clase VehiculoParasigmas

--- src/VehiculoParasigmas.kt
+++ src/VehiculoParasigmas.kt
@@ -4,16 +4,23 @@ class VehiculoParasigmas (
     combustible: Int,
     aceleracion: Int,
     frenado: Int,
+    var velocidad:Int = 0
 ): Vehiculo(nombre, velocidadMaxima, combustible, aceleracion, frenado) {
 
     override fun acelerar() {
         if (combustible!=0){
-
+            while ((velocidad<=velocidadMaxima) and (combustible>0)){
+                velocidad+=aceleracion
+                combustible-=1
+            }
+        }
+        else{
+            println("Ups, ¡no hay combustible!")
         }
     }
 
     override fun frenar() {
-        TODO("Not yet implemented")
+
     }
 
     override fun avanzar() {
